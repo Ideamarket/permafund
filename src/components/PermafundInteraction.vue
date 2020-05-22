a937a96ccd22cb465f4d4e3671280f7e1adc23be
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-05-22 13:11:00 +0200

    Show spinner in buttons during transaction

--- src/components/PermafundInteraction.vue
+++ src/components/PermafundInteraction.vue
@@ -2,12 +2,12 @@
     <div class="container">
         <q-input filled v-model="address" label="Recipient Address" />
         <div class="btnContainer">
-            <q-btn color="primary" label="Create" class="btn" :disable="buttonsDisabled" @click="createClicked" />
-            <q-btn color="primary" label="Pay Interest" class="btn" :disable="buttonsDisabled" @click="payInterestClicked" />
+            <q-btn color="primary" label="Create" class="btn" :loading="createLoading" :disable="buttonsDisabled" @click="createClicked" />
+            <q-btn color="primary" label="Pay Interest" class="btn" :loading="payInterestLoading" :disable="buttonsDisabled" @click="payInterestClicked" />
         </div>
         <q-input filled v-model="donateAmount" label="Donation in DAI" style="margin-top: 45px;"/>
         <div class="btnContainer">
-            <q-btn color="primary" label="Donate" class="btn" :disable="buttonsDisabled" @click="donateClicked" />
+            <q-btn color="primary" label="Donate" class="btn" :loading="donateLoading" :disable="buttonsDisabled" @click="donateClicked" />
         </div>
     </div>
 </template>
@@ -19,12 +19,16 @@ export default {
     return {
       address: '',
       donateAmount: '',
+      createLoading: false,
+      payInterestLoading: false,
+      donateLoading: false,
       buttonsDisabled: false
     }
   },
   methods: {
     createClicked: function () {
       this.buttonsDisabled = true
+      this.createLoading = true
       this.$store.dispatch('permafund/create', this.address)
         .then(() => {
           this.$q.notify({
@@ -46,10 +50,12 @@ export default {
         })
         .finally(() => {
           this.buttonsDisabled = false
+          this.createLoading = false
         })
     },
     donateClicked: function () {
       this.buttonsDisabled = true
+      this.payInterestLoading = true
       this.$store.dispatch('permafund/donate', {
         recipientAddress: this.address,
         amount: this.donateAmount
@@ -74,10 +80,12 @@ export default {
         })
         .finally(() => {
           this.buttonsDisabled = false
+          this.payInterestLoading = false
         })
     },
     payInterestClicked: function () {
       this.buttonsDisabled = true
+      this.donateLoading = true
       this.$store.dispatch('permafund/payInterest', this.address)
         .then(() => {
           this.$q.notify({
@@ -99,6 +107,7 @@ export default {
         })
         .finally(() => {
           this.buttonsDisabled = false
+          this.donateLoading = false
         })
     }
   }
